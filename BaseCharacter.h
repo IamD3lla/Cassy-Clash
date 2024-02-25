c44547db8f7e8011f1350c05d128cc38cf731271
IamD3lla <jvictordgg@gmail.com>
2024-02-25 14:27:23 -0300

    Inheritance

---
+++ BaseCharacter.h
@@ -0,0 +1,14 @@
+//if the name is not defined, will create the code
+#ifndef BASE_CHARACTER_H
+#define BASE_CHARACTER_H
+
+#include "raylib.h"
+
+class BaseCharacter
+{
+    public:
+        BaseCharacter();
+    private:
+};
+
+#endif
